d658e3f0ebca52720d00395124ddceb5451f0f64
Ieva Vyliaudaite <ievavyliaudaite@Ieva-MacBook-Pro.local>
2025-10-01 15:00:00 +0300

    remove trailing slash in corsmiddleware

--- app/main.py
+++ app/main.py
@@ -17,8 +17,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000", 
         "http://localhost:3001", 
-        "https://react-image-generator-kappa.vercel.app/", 
-        "http://172.20.10.5:3000/"],
+        "https://react-image-generator-kappa.vercel.app", 
+        "http://172.20.10.5:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
